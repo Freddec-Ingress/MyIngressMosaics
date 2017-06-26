213ffe13481edf1185d642f44954329eaf57bb0b
freddec <deconinck.frederic@yahoo.fr>
2017-06-26 15:11:48 +0000

    dev

--- api/models.py
+++ api/models.py
@@ -15,7 +15,7 @@ def getDistanceFromLatLng(lat1, lng1, lat2, lng2):
     dLat = radians(lat2 - lat1);
     dLng = radians(lng2 - lng1);
     
-    a =  sin(dLat/2) * sin(dLat/2) + cos(radians(lat1)) * cos(radians(lat2)) *  sin(dLon/2) * sin(dLon/2)
+    a =  sin(dLat/2) * sin(dLat/2) + cos(radians(lat1)) * cos(radians(lat2)) *  sin(dLng/2) * sin(dLng/2)
 
     c = 2 * atan2(sqrt(a), sqrt(1-a)); 
     d = R * c;
