7946f99d75ead1fef43531e1d7cfdb845fff1161
freddec <deconinck.frederic@yahoo.fr>
2017-06-26 09:12:35 +0000

    dev

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
