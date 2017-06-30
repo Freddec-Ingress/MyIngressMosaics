8bf84cf3347e1773adb7c3bb5af31c5f96ff75b9
freddec <deconinck.frederic@yahoo.fr>
2017-06-30 14:53:58 +0000

    dev

--- api/models.py
+++ api/models.py
@@ -132,10 +132,10 @@ class Mission(models.Model):
 
     mosaic = models.ForeignKey('Mosaic', on_delete=models.SET_NULL, null=True, blank=True, related_name='missions')
     
-    data = models.CharField(max_length=4096)
+    data = models.TextField()
 
     ref = models.CharField(max_length=64, default='')
-    desc = models.CharField(max_length=1024, default='')
+    desc = models.TextField(default='')
     title = models.CharField(max_length=128, default='')
     image = models.CharField(max_length=256, default='')
     order = models.IntegerField(default=0)
