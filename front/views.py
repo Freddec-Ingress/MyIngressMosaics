98103523832c568c6229eda8fe83a03be77ecfbb
freddec <deconinck.frederic@yahoo.fr>
2017-09-01 18:42:58 +0000

    dev

--- front/views.py
+++ front/views.py
@@ -70,7 +70,7 @@ def preview(request, ref):
 		order = m.order - 1
 		
 		y = int(order / mosaic.cols)
-		x = y + (order - (y * mosaic.cols))
+		x = (y - 1) + (order - (y * mosaic.cols))
 		
 		xoffset = paddingX + (x * 100)
 		yoffset = paddingY + (y * 100)
