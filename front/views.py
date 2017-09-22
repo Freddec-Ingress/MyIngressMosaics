fc35acb0fa9b0a188858a75d30053683cc06c105
freddec <deconinck.frederic@yahoo.fr>
2017-09-22 09:13:58 +0000

    dev

--- front/views.py
+++ front/views.py
@@ -131,7 +131,7 @@ def preview(request, ref):
 	
 	import boto3
 	s3 = boto3.resource('s3')
-	s3.Bucket('myingressmosaics').put_object(Key='test.jpg', Body=response)
+	s3.Bucket('myingressmosaics').put_object(Key='test.jpg', Body=image)
 	
 	return response
 
