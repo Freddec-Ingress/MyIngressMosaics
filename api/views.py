cb24c88a53b051e0ee6a05dcc1838187f28a0da0
freddec <deconinck.frederic@yahoo.fr>
2017-09-25 09:05:23 +0000

    dev

--- api/views.py
+++ api/views.py
@@ -234,6 +234,23 @@ def mosaic_create(request):
 #---------------------------------------------------------------------------------------------------
 @api_view(['GET'])
 @permission_classes((AllowAny, ))
+def mosaic_preview(request, ref):
+	
+	data = None
+	
+	result = Mosaic.objects.filter(ref=ref)
+	if result.count() > 0:
+		
+		mosaic = result[0]
+		mosaic.generatePreview()
+	
+	return Response(data, status=status.HTTP_200_OK)
+		
+	
+	
+#---------------------------------------------------------------------------------------------------
+@api_view(['GET'])
+@permission_classes((AllowAny, ))
 def mosaic_view(request, ref):
 	
 	data = None
