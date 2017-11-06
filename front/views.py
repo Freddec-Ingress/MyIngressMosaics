75deb669d8f7df13ba3500b8406887417d735193
freddec <deconinck.frederic@yahoo.fr>
2017-11-06 08:20:50 +0000

    dev

--- front/views.py
+++ front/views.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 
 from django.http import HttpResponse
 
+import re
 import math, os
 import urllib, io
 
@@ -228,7 +229,7 @@ def city(request, country, region, city):
 
 def region(request, country, region):
 	
-	context = { 'country':country, 'region':region }
+	context = { 'country':country, 'region':re.escape(region) }
 	return render(request, 'region.html', context)
 
 
