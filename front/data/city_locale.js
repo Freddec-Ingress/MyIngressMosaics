80f119d2e980f04253e287fff47b7574df1c9a7f
freddec <deconinck.frederic@yahoo.fr>
2017-11-08 14:34:18 +0000

    dev

--- front/data/city_locale.js
+++ front/data/city_locale.js
@@ -1,1 +1,27 @@
 var cityLocaleLabelMap = new Map();
+
+/* Japan,Tokyo */
+
+cityLocaleLabelMap.set('Japan,Tokyo,Chiyoda', '千代田区');
+cityLocaleLabelMap.set('Japan,Tokyo,Chūō', '中央区');
+cityLocaleLabelMap.set('Japan,Tokyo,Minato', '港区');
+cityLocaleLabelMap.set('Japan,Tokyo,Shinjuku', '新宿区');
+cityLocaleLabelMap.set('Japan,Tokyo,Bunkyō', '文京区');
+cityLocaleLabelMap.set('Japan,Tokyo,Taitō', '台東区');
+cityLocaleLabelMap.set('Japan,Tokyo,Sumida', '墨田区');
+cityLocaleLabelMap.set('Japan,Tokyo,Kōtō', '江東区');
+cityLocaleLabelMap.set('Japan,Tokyo,Shinagawa', '品川区');
+cityLocaleLabelMap.set('Japan,Tokyo,Meguro', '目黒区');
+cityLocaleLabelMap.set('Japan,Tokyo,Ōta', '大田区');
+cityLocaleLabelMap.set('Japan,Tokyo,Setagaya', '世田谷区');
+cityLocaleLabelMap.set('Japan,Tokyo,Shibuya', '渋谷区');
+cityLocaleLabelMap.set('Japan,Tokyo,Nakano', '中野区');
+cityLocaleLabelMap.set('Japan,Tokyo,Suginami', '杉並区');
+cityLocaleLabelMap.set('Japan,Tokyo,Toshima', '豊島区');
+cityLocaleLabelMap.set('Japan,Tokyo,Kita', '北区');
+cityLocaleLabelMap.set('Japan,Tokyo,Arakawa', '荒川区');
+cityLocaleLabelMap.set('Japan,Tokyo,Itabashi', '板橋区');
+cityLocaleLabelMap.set('Japan,Tokyo,Nerima', '練馬区');
+cityLocaleLabelMap.set('Japan,Tokyo,Adachi', '足立区');
+cityLocaleLabelMap.set('Japan,Tokyo,Katsushika', '葛飾区');
+cityLocaleLabelMap.set('Japan,Tokyo,Edogawa', '江戸川区');
