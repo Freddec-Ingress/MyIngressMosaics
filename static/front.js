66af89403f09accba7cfd982059004b3afd88b4f
freddec <deconinck.frederic@yahoo.fr>
2017-11-09 21:50:23 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -1983,6 +1983,62 @@ angular.module('FrontModule.controllers').controller('RegistrationCtrl', functio
 							if (!potential.city && admin3) potential.city = item.admin3;
 							
 							UtilsService.checkMosaicLocations(potential);
+						
+							/* Country */
+							
+							var cur_country = null;
+							for (var country of countryList) {
+								if (country.name == potential.country || country.locale == potential.country) {
+									
+									cur_country = country;
+									
+									potential.country = country.name;
+									break;
+								}
+							}
+							
+							/* Region */
+							
+							if (cur_country) {
+								
+								var data = {'country_id':cur_country.id};
+								API.sendRequest('/api/region/list/', 'POST', {}, data).then(function(response) {
+									
+									var regionList = response.regions;
+									
+									var cur_region = null;
+									for (var region of regionList) {
+										if (region.name == potential.region || region.locale == potential.region) {
+											
+											cur_region = region;
+											
+											potential.region = region.name;
+											break;
+										}
+									}
+									
+									/* City */
+									
+									if (cur_region) {
+										
+										var data = {'country_id':cur_country.id, 'region_id':cur_region.id};
+										API.sendRequest('/api/city/list/', 'POST', {}, data).then(function(response) {
+											
+											var cityList = response.cities;
+											
+											var cur_city = null;
+											for (var city of cityList) {
+												if (city.name == potential.city || city.locale == potential.city) {
+													
+													cur_city = city;
+													
+													potential.city = city.name;
+												}
+											}
+										});
+									}
+								});
+							}
 							
 							$scope.$applyAsync();
 						}
