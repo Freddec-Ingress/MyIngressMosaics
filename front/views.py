b4e23ba1bc1a5a8c32b993a4665983fcbca68e23
freddec <deconinck.frederic@yahoo.fr>
2017-11-09 14:00:54 +0000

    dev

--- front/views.py
+++ front/views.py
@@ -257,31 +257,6 @@ def recruitment(request):
 	
 def migrate(request):
 	
-	Country.objects.all().delete()
-	Region.objects.all().delete()
-	City.objects.all().delete()
-	
-	country_name_results = Mosaic.objects.values('country').distinct()
-	if (country_name_results.count() > 0):
-		
-		for country_name in country_name_results:
-			country = Country(name=country_name['country'])
-			country.save()
-			
-			region_name_results = Mosaic.objects.filter(country=country_name['country']).values('region').distinct()
-			if (region_name_results.count() > 0):
-				
-				for region_name in region_name_results:
-					region = Region(country=country, name=region_name['region'])
-					region.save()
-					
-					city_name_results = Mosaic.objects.filter(country=country_name['country'], region=region_name['region']).values('city').distinct()
-					if (city_name_results.count() > 0):
-						
-						for city_name in city_name_results:
-							city = City(country=country, region=region, name=city_name['city'])
-							city.save()
-							
 	results = Mosaic.objects.all()
 	for item in results:
 		
