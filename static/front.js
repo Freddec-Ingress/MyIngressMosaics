c56fb70e6d90c6a1291b37a6f5fb33bfc3508e7f
freddec <deconinck.frederic@yahoo.fr>
2017-11-22 16:03:51 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -2846,7 +2846,7 @@ angular.module('FrontModule.controllers').controller('NewMosaicCtrl', function($
 		});
 	}
 });
-angular.module('FrontModule.controllers').controller('NewRegistrationCtrl', function($scope, $window, toastr, API) {
+angular.module('FrontModule.controllers').controller('NewRegistrationCtrl', function($scope, $window, toastr, API, UtilsService) {
 	
 	/* Tab management */
 	
@@ -2857,7 +2857,55 @@ angular.module('FrontModule.controllers').controller('NewRegistrationCtrl', func
 		$scope.current_step = id;
 	}
 	
+	/* Step #1 management */
+	
+	$scope.missions = [];
+	$scope.filtered = [];
+	
+	$scope.refreshing = false;
+	
+	$scope.filter = '';
+	
+	$scope.refreshMissions = function() {
+		
+		$scope.refreshing = true;
+		
+		$scope.missions = [];
+		$scope.filtered = [];
+		
+		API.sendRequest('/api/missions/', 'POST').then(function(response) {
+			
+			$scope.missions = response.missions;
+			if (!$scope.missions) $scope.missions = [];
+
+			$scope.missions.sort(UtilsService.sortMissionsByCreatorTitleAsc);
+
+			$scope.filterMissions($scope.filter);
+			
+			$scope.refreshing = false;
+		});
+	}
+	
+	$scope.filterMissions = function(text) {
+		
+		$scope.filter = text;
+		$scope.filtered = [];
+		
+		if (!$scope.filter) $scope.filter = $scope.missions.slice();
+		else  {
+			
+			for (var item of $scope.missions) {
+				
+				if (item.title.indexOf(text) != -1 || item.creator.indexOf(text) != -1) {
+					$scope.filtered.push(item);
+				}
+			}
+		}
+	}
+	
 	/* Page loading */
+
+	$scope.refreshMissions();
 	
 	$scope.open_step(1);
 	
