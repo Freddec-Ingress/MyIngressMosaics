45a39fc5ae9b4cb664350992220635bcf909ff3f
freddec <deconinck.frederic@yahoo.fr>
2017-11-24 16:28:41 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -3237,7 +3237,33 @@ angular.module('FrontModule.controllers').controller('NewCountryCtrl', function(
 	
 	$scope.loaded = true;
 });
-angular.module('FrontModule.controllers').controller('NewLoginCtrl', function($scope, $window, API) {
+angular.module('FrontModule.controllers').controller('NewLoginCtrl', function($scope, $window, $auth, $cookies, API) {
+	
+	/* Login management */
+	
+	$scope.socialLogin = function(provider) {
+			
+		$auth.authenticate(provider).then(function(response) {
+			
+			$auth.setToken(response.data.token);
+			$cookies.token = response.data.token;
+			
+			$window.location.href = '/';
+		});
+	}
+	
+	$scope.localLogin = function(username, password) {
+
+		var data = { 'username':username, 'password':password }
+		API.sendRequest('/api/user/login/', 'POST', {}, data).then(function(response) {
+			
+			$auth.setToken(response.token);
+			$cookies.token = response.token;
+			
+			$window.location.href = '/';
+			
+		});
+	}
 	
 	/* Page loading */
 	
