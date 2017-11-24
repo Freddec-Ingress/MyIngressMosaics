e09a9029fe8d41f11acaeacf80abb1525297659b
freddec <deconinck.frederic@yahoo.fr>
2017-11-24 12:35:24 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -3395,43 +3395,10 @@ angular.module('FrontModule.controllers').controller('NewMapCtrl', function($sco
 												if (response) {
 													
 													var details = response[0];
-												
-													var contentImage = '';
-													for (var m of details.missions.reverse()) {
-														
-														contentImage +=	
-														'<div style="flex:0 0 calc(100% / ' + details.cols + ');">' +
-														'	<img src="/static/img/mask.png" style="width:100%; background-color:#000000; background-image:url(' + m.image + '=s100); background-size: 95% 95%; background-position: 50% 50%; float:left; background-repeat: no-repeat;" />' +
-														'</div>'
-														;
-													}
-													
-													var contentClass = '';
-													if (details.missions.length > 24) contentClass = 'scrollbar valign-start';
-													if (details.missions.length <= 24) contentClass = 'valign-center';
-		
-													var contentDistance = '';
-													if (details.type == 'sequence') contentDistance = details.distance.toFixed(2).toString() + ' km';
-													if (details.type == 'serie') contentDistance = 'serie';
-													if (details.type == 'sequence' && details.distance > 10.0 && details.distance < 30.0) contentDistance += '<span class="text-separator">&middot;</span><i class="fa fa-bicycle"></i>';
-													if (details.type == 'sequence' && details.distance > 30.0) contentDistance += '<span class="text-separator">&middot;</span><i class="fa fa-car"></i>';
 													
 													var contentString =
-														'<a class="btn-primary btn-block ta-left ttrans-normal" style="width:170px; font: 400 .75rem/1.5 \'Coda\',sans-serif;" href="/mosaic/' + details.ref + '">' +
-															
-														'	<div class="item ' + contentClass + '" style="margin-bottom:.25rem; display:flex; justify-content:center; background:#0b0c0d; height:105px; overflow-y:hidden; padding:.25rem;">' +
-																
-														'		<div class="row" style="align-items:center; justify-content:center; padding:0 calc((6 - ' + details.cols + ') / 2 * 16.666667%); width:100%;">' + contentImage + '</div>' +
-																
-														'	</div>' +
-															
-												        '    	<div class="ellipsis" style="margin-bottom:.25rem;">' + details.title + '</div>' +
-													        '   <div class="c-lighter ellipsis">' +
-													        '       <flag country="\'' + details.country + '\'"></flag>' +
-													        details.location +
-													        '   </div>' +
-												        '    	<div class="c-lighter">' + details.missions.length + ' <i class="fa fa-th"></i> <span class="text-separator">&middot;</span>' + contentDistance + '</div>' +
-		
+														'<a class="flex-col" style="font: 400 .75rem/1.5 \'Coda\',sans-serif;" href="/mosaic/' + details.ref + '">' +
+												        '	<span class="text-big">' + details.title + '</span>' +
 														'</a>' +
 													'';
 		
