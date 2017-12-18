0e54243ddce9d333a8095158e3d2a26a6693a60d
freddec <deconinck.frederic@yahoo.fr>
2017-12-18 12:06:42 +0000

    dev

--- front/urls.py
+++ front/urls.py
@@ -29,9 +29,9 @@ urlpatterns = [
 	
 	url(r'^register',							views.register),
 	
-	url(r'^world/(?P<country>[\w \-,\']+)/(?P<region>[\w|\W \-,\']+)/(?P<city>[\w|\W \-,\']+)',	views.city),
-	url(r'^world/(?P<country>[\w \-,\']+)/(?P<region>[\w|\W \-,\']+)',	views.region),
-	url(r'^world/(?P<country>[\w \-,\']+)', 	views.country),
+	url(r'^world/(?P<country>[\w \-,.\']+)/(?P<region>[\w|\W \-,.\']+)/(?P<city>[\w|\W \-,.\']+)',	views.city),
+	url(r'^world/(?P<country>[\w \-,.\']+)/(?P<region>[\w|\W \-,.\']+)',	views.region),
+	url(r'^world/(?P<country>[\w \-,.\']+)', 	views.country),
 	url(r'^world',								views.world),
 	
 	url(r'^recruitment',						views.recruitment),
