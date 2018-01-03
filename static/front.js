2eb5e762ad5a74997d88d82ab95f53dcea62169f
freddec <deconinck.frederic@yahoo.fr>
2018-01-03 10:32:51 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -3640,6 +3640,7 @@ angular.module('FrontModule.controllers').controller('AdmRegistationCtrl', funct
 			var index = $scope.mosaics.indexOf(mosaic);
 			$scope.mosaics.splice(index, 1);
 			
+			console.log(index);
 			$scope.openMosaic($scope.mosaics[index], index-1);
 		});
 	}
