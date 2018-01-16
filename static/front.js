c836afd4a18bdbae34d76f40aea14bb41cef72b4
freddec <deconinck.frederic@yahoo.fr>
2018-01-16 22:39:19 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -3800,6 +3800,40 @@ angular.module('FrontModule.controllers').controller('AdmMosaicCtrl', function($
 	    $scope.loaded = true;
 	}
 });
+angular.module('FrontModule.controllers').controller('AdmMissionCtrl', function($scope, API) {
+    
+	/* Mission management */
+	
+	$scope.loading = false;
+	
+	$scope.selectMission = function(mission_ref) {
+	    
+	    $scope.loading = true;
+	    
+	    var data = { 'ref':mission_ref };
+		API.sendRequest('/api/mission/details/', 'POST', {}, data).then(function(response) {
+            
+            $scope.mission = response;
+            $scope.loading = false;
+        });
+	}
+	
+	$scope.updateMission = function(mission) {
+	    
+	    $scope.loading = true;
+	    
+	    var data = { 'ref':mission.ref, 'name':mission.name };
+		API.sendRequest('/api/mission/update/', 'POST', {}, data).then(function(response) {
+            
+            $scope.mission = response;
+            $scope.loading = false;
+        });
+	}
+	
+	/* Page loading */
+	
+    $scope.loaded = true;
+});
 angular.module('FrontModule', ['satellizer', 'ngCookies', 'toastr',
 							   'FrontModule.services', 'FrontModule.controllers', 'FrontModule.directives', ]);
 
