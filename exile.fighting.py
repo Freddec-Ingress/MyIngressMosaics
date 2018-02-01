da84230ad09f8605d55717e650aff4dc5d84528f
freddec <deconinck.frederic@yahoo.fr>
2018-02-01 16:54:26 +0000

    dev

---
+++ exile.fighting.py
@@ -0,0 +1,7 @@
+Pour chaque flotte en combat,
+    je répertorie les types de vaisseaux adverses
+    je calcul le tir pour chaque type de vaisseaux adverses (ex: je tir pour détruire 100 chasseurs)
+    je répartie le tir sur le nombre de flotte adverse en fonction du type de vaisseaux (ex: si j'ai 4 adversaires alors chaque adversaires predra 25 chasseurs)
+    
+Pour chaque flotte en combat,
+     je calcule les tirs reçus
