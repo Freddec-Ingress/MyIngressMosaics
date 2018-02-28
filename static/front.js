c315ed4e4704866464d5d8a6a7897bbb88b7b601
freddec <deconinck.frederic@yahoo.fr>
2018-02-28 08:51:48 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -3516,8 +3516,8 @@ angular.module('FrontModule.controllers').controller('NewCityCtrl', function($sc
 		
 		$scope.mosaics.sort(function(a, b) {
 			
-			if (a.title > b.title) return -1;
-			if (a.title < b.title) return 1;
+			if (a.title > b.title) return 1;
+			if (a.title < b.title) return -1;
 			
 			return 0;
 		});
