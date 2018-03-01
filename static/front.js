bea6d7d8940503eb29f480eedded362e9957eaff
freddec <deconinck.frederic@yahoo.fr>
2018-03-01 11:24:17 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -3186,6 +3186,8 @@ angular.module('FrontModule.controllers').controller('NewMapCtrl', function($sco
 				var tilesProcessed = [];
 				var tilesToBeProcessed = [];
 				
+				var tilesPerEdgeArray = [80,320,1000,2000,2000,4000,8000,16000,16000,32000];
+				
 				map.addListener('idle', function(e) {
 					
 					$scope.mosaics = [];
@@ -3200,123 +3202,117 @@ angular.module('FrontModule.controllers').controller('NewMapCtrl', function($sco
 					var North_Lat = bds.getNorthEast().lat();
 					var North_Lng = bds.getNorthEast().lng();
 					
-					var tilesPerEdge = 2000;
-			
-			        var xStart = Math.floor((South_Lng + 180) / 360 * tilesPerEdge);
-			        var xEnd = Math.floor((North_Lng + 180) / 360 * tilesPerEdge);
-			
-			        var yStart = Math.floor((1 - Math.log(Math.tan(North_Lat * Math.PI / 180) + 1 / Math.cos(North_Lat * Math.PI / 180)) / Math.PI) / 2 * tilesPerEdge);
-			        var yEnd = Math.floor((1 - Math.log(Math.tan(South_Lat * Math.PI / 180) + 1 / Math.cos(South_Lat * Math.PI / 180)) / Math.PI) / 2 * tilesPerEdge);
-			        
-			        for (var x = xStart; x <= xEnd; x++) {
-			            for (var y = yStart; y <= yEnd; y++) {
-							
-							var tile_id = x + '_' + y;
-			                if (tilesProcessed.indexOf(tile_id) == -1 && tilesToBeProcessed.indexOf(tile_id) == -1) {
-							
-								tilesToBeProcessed.push(tile_id);
-							
-			                    var south = tileToLat(y + 1, tilesPerEdge);
-			                    var north = tileToLat(y, tilesPerEdge);
-			                    var west = tileToLng(x, tilesPerEdge);
-			                    var east = tileToLng(x + 1, tilesPerEdge);
-			                    
-								var data = {'sLat':south, 'sLng':west, 'nLat':north, 'nLng':east};
-								API.sendRequest('/api/map/', 'POST', {}, data).then(function(response) {
-									
-									if (response) {
+					var zoom = map.getZoom();
+					if (zoom > 10) {
+						
+						var tilesPerEdge = tilesPerEdgeArray[zoom-10];
+				
+				        var xStart = Math.floor((South_Lng + 180) / 360 * tilesPerEdge);
+				        var xEnd = Math.floor((North_Lng + 180) / 360 * tilesPerEdge);
+				
+				        var yStart = Math.floor((1 - Math.log(Math.tan(North_Lat * Math.PI / 180) + 1 / Math.cos(North_Lat * Math.PI / 180)) / Math.PI) / 2 * tilesPerEdge);
+				        var yEnd = Math.floor((1 - Math.log(Math.tan(South_Lat * Math.PI / 180) + 1 / Math.cos(South_Lat * Math.PI / 180)) / Math.PI) / 2 * tilesPerEdge);
+				        
+				        for (var x = xStart; x <= xEnd; x++) {
+				            for (var y = yStart; y <= yEnd; y++) {
+								
+								var tile_id = x + '_' + y;
+				                if (tilesProcessed.indexOf(tile_id) == -1 && tilesToBeProcessed.indexOf(tile_id) == -1) {
+								
+									tilesToBeProcessed.push(tile_id);
+								
+				                    var south = tileToLat(y + 1, tilesPerEdge);
+				                    var north = tileToLat(y, tilesPerEdge);
+				                    var west = tileToLng(x, tilesPerEdge);
+				                    var east = tileToLng(x + 1, tilesPerEdge);
+				                    
+									var data = {'sLat':south, 'sLng':west, 'nLat':north, 'nLng':east};
+									API.sendRequest('/api/map/', 'POST', {}, data).then(function(response) {
 										
-										tilesProcessed.push(tile_id);
-										
-										$scope.mosaics = response;
-										
-										for (var item of response) {
-										
-											var index = refArray.indexOf(item.ref)
+										if (response) {
 											
-											if (index == -1) {
-					
-												var latLng = new google.maps.LatLng(item.startLat, item.startLng);
-												var marker = new google.maps.Marker({
-													position: latLng,
-													map: map,
-													icon: image,
-												});
+											tilesProcessed.push(tile_id);
+											
+											$scope.mosaics.append(response);
+											
+											for (var item of response) {
+											
+												var index = refArray.indexOf(item.ref)
 												
-												google.maps.event.addListener(marker, 'click', (function (marker, mosaic, infowindow) {
+												if (index == -1) {
+						
+													var latLng = new google.maps.LatLng(item.startLat, item.startLng);
+													var marker = new google.maps.Marker({
+														position: latLng,
+														map: map,
+														icon: image,
+													});
 													
-													return function () {
+													google.maps.event.addListener(marker, 'click', (function (marker, mosaic, infowindow) {
 														
-														var offset_string = '';
-														
-														var temp = 0;
-														if (mosaic.missions.length > mosaic.cols) {
-															temp = mosaic.cols - mosaic.missions.length % mosaic.cols;
-															if (temp < 0 || temp > (mosaic.cols - 1)) temp = 0;
-														}
-														
-														for (var i = 0; i < temp; i++) {
-															offset_string += '<div style="flex:0 0 calc(100% / ' + mosaic.cols + ');"></div>';
-														}
-														
-														var missions_string = '';
-														
-														var missions_array = mosaic.missions.slice();
-														for (var mission of missions_array.reverse()) {
+														return function () {
 															
-															missions_string +=
-													            '<div class="mission-vignet" style="flex:0 0 calc(100% / ' + mosaic.cols + ');">' +
-													                '<img src="/static/img/mask.png" style="z-index:auto; background-image:url(' + mission.image + '=s100);" />' +
-													            '</div>';
-														}
+															var offset_string = '';
+															
+															var temp = 0;
+															if (mosaic.missions.length > mosaic.cols) {
+																temp = mosaic.cols - mosaic.missions.length % mosaic.cols;
+																if (temp < 0 || temp > (mosaic.cols - 1)) temp = 0;
+															}
+															
+															for (var i = 0; i < temp; i++) {
+																offset_string += '<div style="flex:0 0 calc(100% / ' + mosaic.cols + ');"></div>';
+															}
+															
+															var missions_string = '';
+															
+															var missions_array = mosaic.missions.slice();
+															for (var mission of missions_array.reverse()) {
+																
+																missions_string +=
+														            '<div class="mission-vignet" style="flex:0 0 calc(100% / ' + mosaic.cols + ');">' +
+														                '<img src="/static/img/mask.png" style="z-index:auto; background-image:url(' + mission.image + '=s100);" />' +
+														            '</div>';
+															}
+															
+															var contentString = '' +
+																'<a class="flex-col" target="_blank" style="width:200px; min-width:200px; max-width:200px;" href="/mosaic/' + mosaic.ref + '" >' +
+																	'<div class="flex-col" style="flex-shrink:1;">' + 
+																		'<span class="color-black text-medium text-bold" style="word-break: break-all;">' + mosaic.title + '</span>' + 
+																		'<span class="color-grey mb-small">' + mosaic.missions.length + ' missions</span>' + 
+																	'</div>' + 
+																	'<div style="max-height:300px; overflow-y:auto;">' +
+																		'<div class="flex wrap shrink justify-center" style="padding:0 calc((6 - ' + mosaic.cols + ') / 2 * 16.666667%);">' +
+																			offset_string +
+																			missions_string + 
+																		'</div>' +
+																	'</div>' + 
+																'</a>';
+																'';
+															
+															var contentDiv = angular.element('<div/>');
+															contentDiv.append(contentString);													
+															
+															var compiledContent = $compile(contentDiv)($scope);
+															
+															infowindow.setContent(compiledContent[0]);
+															infowindow.open($scope.map, marker);
+														};
 														
-														var contentString = '' +
-															'<a class="flex-col" target="_blank" style="width:200px; min-width:200px; max-width:200px;" href="/mosaic/' + mosaic.ref + '" >' +
-																'<div class="flex-col" style="flex-shrink:1;">' + 
-																	'<span class="color-black text-medium text-bold" style="word-break: break-all;">' + mosaic.title + '</span>' + 
-																	'<span class="color-grey mb-small">' + mosaic.missions.length + ' missions</span>' + 
-																'</div>' + 
-																'<div style="max-height:300px; overflow-y:auto;">' +
-																	'<div class="flex wrap shrink justify-center" style="padding:0 calc((6 - ' + mosaic.cols + ') / 2 * 16.666667%);">' +
-																		offset_string +
-																		missions_string + 
-																	'</div>' +
-																'</div>' + 
-															'</a>';
-															'';
-														
-														var contentDiv = angular.element('<div/>');
-														contentDiv.append(contentString);													
-														
-														var compiledContent = $compile(contentDiv)($scope);
-														
-														infowindow.setContent(compiledContent[0]);
-														infowindow.open($scope.map, marker);
-													};
+													})(marker, item, infowindow));
 													
-												})(marker, item, infowindow));
-												
-												refArray.push(item.ref);
-												markerArray.push(marker);
+													refArray.push(item.ref);
+													markerArray.push(marker);
+												}
 											}
+											
+											if (tilesToBeProcessed.length < 1) $scope.flag_loading = false;
 										}
-										
-										var markerCluster = new MarkerClusterer(map, markerArray, { imagePath: 'https://www.myingressmosaics.com/static/img/m' });
-										markerCluster.setMaxZoom(10);
-										
-										$scope.flag_loading = false;
-									}
-									else {
-										
-										$scope.mosaics = [];
-										
-										$scope.flag_no_mosaic = true;
-										$scope.flag_loading = false;
-									}
-								});
-			                }
-			            }
-			        }
+									});
+				                }
+				            }
+				        }
+					}
 				});
 		
 		    	document.getElementById('submit').addEventListener('click', function() {
