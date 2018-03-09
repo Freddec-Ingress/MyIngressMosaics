0b8ca7408caaf1c252bd1f67606a1294ffb80bcf
freddec <deconinck.frederic@yahoo.fr>
2018-03-09 10:45:51 +0000

    dev

--- api/views.py
+++ api/views.py
@@ -1802,10 +1802,10 @@ def telegram_updates(request):
 		results = City.objects.filter(name__iexact=city_query)
 		if results.count() > 0:
 			city_data = results[0]
-			print(city_data)
 			response_txt += '<div><span>' + city_data.name + ', ' + city_data.region.name + ', ' + city_data.country.name + '</span></div>'
 			response_txt += '<div><span>' + str(city_data.mosaics.count()) + ' mosaics</span></div>'
 			response_txt += '<div><a href="https://www.myingressmosaics.com/world/' + city_data.country.name + '/' + city_data.region.name + '/' + city_data.name + '">MIM link</a></div>'
+			print(response_txt)
 		else:
 			response_txt = 'No city found (' + city_query + ')'
 	
