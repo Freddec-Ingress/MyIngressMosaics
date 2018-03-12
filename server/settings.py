fb51b345d4bbb46d642503f484638e5705f20145
freddec <deconinck.frederic@yahoo.fr>
2018-03-12 12:52:23 +0000

    dev

--- server/settings.py
+++ server/settings.py
@@ -186,8 +186,11 @@ LOGGING = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_PORT = 587
+
 EMAIL_HOST = 'ssl0.ovh.net'
 EMAIL_HOST_USER = 'admin@myingressmosaics.com'
 EMAIL_HOST_PASSWORD = 'freddec@2012'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = False
+
+EMAIL_USE_TLS = True
