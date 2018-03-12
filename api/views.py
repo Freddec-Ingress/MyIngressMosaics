973727a6e4f27080ccc8fe1f9274d18d5b08b90b
freddec <deconinck.frederic@yahoo.fr>
2018-03-12 14:02:29 +0000

    dev

--- api/views.py
+++ api/views.py
@@ -430,7 +430,10 @@ def mosaic_create(request):
 	region_notifiers = Notif.objects.filter(country=country, region=region, city__isnull=True).values_list('user__email')
 	city_notifiers = Notif.objects.filter(country=country, region=region, city=city).values_list('user__email')
 	
-	receivers = list(list(country_notifiers) + list(region_notifiers) + list(city_notifiers))
+	receivers = []
+	for item in country_notifiers: receivers.append(item)
+	for item in region_notifiers: receivers.append(item)
+	for item in city_notifiers: receivers.append(item)
 	print(receivers)
 	receivers = set(receivers)
 	print(receivers)
