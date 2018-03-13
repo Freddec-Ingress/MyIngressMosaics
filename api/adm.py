ec09e2dff0acd35ae7b4fbbdff6f9e63b9483f95
freddec <deconinck.frederic@yahoo.fr>
2018-03-13 08:47:28 +0000

    dev

--- api/adm.py
+++ api/adm.py
@@ -16,4 +16,21 @@ from .models import *
 def adm_compare(request):
 	
 	data = { 'countries':[] }
+	
+	results = IMCountry.objects.all()
+	for item in results:
+		
+		compare_count = 0
+		compare = Country.objects.filter(name=item.name)
+		if compare.count() > 0:
+			compare = compare[0]
+			compare_count = Mosaic.objects.filter(city__region__country=compare).count()
+		
+		country = {
+			'name': item.name,
+			'diff': item.count - compare_count,
+		}
+		
+		data['countries'].append(country)
+	
 	return Response(data, status=status.HTTP_200_OK)
--- static/front.js
+++ static/front.js
@@ -4385,6 +4385,18 @@ angular.module('FrontModule.controllers').controller('AdmCompareCtrl', function(
 	API.sendRequest('/api/adm/compare/', 'POST').then(function(response) {
 	    
 	    $scope.countries = response.countries;
+	    
+		$scope.countries.sort(function(a, b) {
+			
+			if (a.diff > b.diff) return -1;
+			if (a.diff < b.diff) return 1;
+			
+			if (a.name > b.name) return 1;
+			if (a.name < b.name) return -1;
+			
+			return 0;
+		});
+	    
         $scope.loaded = true;
 	});
 });
