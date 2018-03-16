c125ee841797bffef24198677822ddcc36020c0c
freddec <deconinck.frederic@yahoo.fr>
2018-03-16 13:13:26 +0000

    dev

--- api/adm.py
+++ api/adm.py
@@ -82,13 +82,13 @@ def adm_compare(request):
 					
 						to_add = False
 						if not imm_item.compare_name:
-							m_compare = Mosaic.objects.filter(city__region=r_compare, name=imm_item.name)
+							m_compare = Mosaic.objects.filter(city__region=r_compare, title=imm_item.name)
 							if m_compare.count() > 0:
 								imm_item.compare_name = m_compare.name
 								imm_item.save()
 								to_add = True
 						else:
-							m_compare = Mosaic.objects.filter(city__region=r_compare, name=imm_item.compare_name)
+							m_compare = Mosaic.objects.filter(city__region=r_compare, title=imm_item.compare_name)
 							if m_compare.count() > 0:
 								to_add = True
 						
