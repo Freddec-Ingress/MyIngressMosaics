d0e1dfecb45f108f743f51752bd95a3c92d552ee
freddec <deconinck.frederic@yahoo.fr>
2018-03-25 18:59:46 +0000

    dev

--- api/mosaic.py
+++ api/mosaic.py
@@ -6,6 +6,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
 from .models import *
 
 
