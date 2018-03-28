e338642e75d46dc56d8c4ea963cdd5f26c5a8d0e
freddec <deconinck.frederic@yahoo.fr>
2018-03-28 12:10:40 +0000

    dev

--- front/scripts/adm.checks.js
+++ front/scripts/adm.checks.js
@@ -1,5 +1,9 @@
 angular.module('FrontModule.controllers').controller('AdmChecksCtrl', function($scope, API) {
 	
+	/* Tab management */
+	
+	$scope.current_tab = 'region';
+	
 	/* Page loading */
 	
 	$scope.loaded = true;
