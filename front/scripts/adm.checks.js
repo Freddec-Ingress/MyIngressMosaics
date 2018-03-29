7d972c8a356bd3a5f0ef0b408b4da93ed8485c2a
freddec <deconinck.frederic@yahoo.fr>
2018-03-29 14:07:07 +0000

    dev

--- front/scripts/adm.checks.js
+++ front/scripts/adm.checks.js
@@ -7,12 +7,9 @@ angular.module('FrontModule.controllers').controller('AdmChecksCtrl', function($
 	$scope.preview_total_count = 0;
 	$scope.preview_current_count = 0;
 	
-	var refs = null;
-	
 	var previewCall = function() {
 		
-		var ref = refs[$scope.preview_current_count];
-		API.sendRequest('/api/mosaic/generate/', 'POST', {}, { 'ref':ref }).then(function(response) {
+		API.sendRequest('/api/mosaic/generate/', 'GET').then(function(response) {
 			
 			$scope.preview_current_count += 1;
 			
@@ -41,15 +38,7 @@ angular.module('FrontModule.controllers').controller('AdmChecksCtrl', function($
 		$scope.preview_total_count = 0;
 		$scope.preview_current_count = 0;
 		
-		API.sendRequest('/api/mosaic/getall/', 'POST', {}, null).then(function(response) {
-			
-			refs = response.refs;
-			
-			$scope.preview_generating = true;
-			$scope.preview_total_count = refs.length;
-			
-			previewCall();
-		});
+		previewCall();
 	}
 	
 	/* Tab management */
