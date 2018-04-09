17d13cb3cd3234e914bf8651e0106becabfc1889
freddec <deconinck.frederic@yahoo.fr>
2018-04-09 12:44:32 +0000

    dev

--- front/scripts/ctrl.page.tag.js
+++ front/scripts/ctrl.page.tag.js
@@ -4,7 +4,29 @@ angular.module('FrontModule.controllers').controller('TagPageCtrl', function($sc
 	
 	$scope.init = function(mosaics) {
 	
-		$scope.mosaics = mosaics;
+		$scope.mosaics_sorting=='by_date'
+	
+		$scope.mosaics_by_date = mosaics;
+		$scope.mosaics_by_missions = mosaics;
+		
+		$scope.mosaics_by_date.sort(function(a, b) {
+			
+			if (a.id > b.id) return -1;
+			if (a.id < b.id) return 1;
+			
+			return 0;
+		});
+		
+		$scope.mosaics_by_missions.sort(function(a, b) {
+			
+			if (a.mission_count > b.mission_count) return -1;
+			if (a.mission_count < b.mission_count) return 1;
+			
+			if (a.title > b.title) return 1;
+			if (a.title < b.title) return -1;
+			
+			return 0;
+		});
 		
 		$scope.loaded = true;
 	}
