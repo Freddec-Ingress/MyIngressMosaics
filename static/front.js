4f5f774dba03e1140c7d6334ffa7a41a09853f62
freddec <deconinck.frederic@yahoo.fr>
2018-04-09 14:55:02 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -2851,6 +2851,40 @@ angular.module('FrontModule.controllers').controller('TagPageCtrl', function($sc
 			return 0;
 		});
 		
+		var country_code_array = [];
+		
+		$scope.indexes_by_country = []
+		for (var mosaic of $scope.mosaics_by_date) {
+			
+			var country_code = mosaic.country_code;
+			
+			var index = country_code_array.indexOf(country_code);
+			
+			var index_by_country = null;
+			
+			if (index == -1) {
+				
+				index_by_country = {
+					
+					'code':mosaic.country_code,
+					'name':mosaic.country_name,
+					
+					'mosaics':[],
+				}
+				
+				$scope.indexes_by_country.push(index_by_country);
+			}
+			else {
+				
+				index_by_country = $scope.indexes_by_country[index]
+			}
+			
+			index_by_country.mosaics.push(mosaic);
+		}
+		
+		$scope.current_by_country_index = $scope.indexes_by_country[0];
+		$scope.mosaics_by_country = $scope.current_by_country_index.mosaics;
+		
 		$scope.loaded = true;
 	}
 });
