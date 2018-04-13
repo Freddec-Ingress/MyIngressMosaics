3f290912953586e1aafca23abb97493d763d480c
freddec <deconinck.frederic@yahoo.fr>
2018-04-13 12:10:30 +0000

    dev

--- front/views.py
+++ front/views.py
@@ -383,12 +383,12 @@ def search(request, search_string = ''):
 	
 #---------------------------------------------------------------------------------------------------
 def tag(request, tag):
-	'''	
-	mosaic_results = Mosaic.objects.filter(tags__icontains='MPEarthDay')
+	
+	mosaic_results = Mosaic.objects.filter(title__icontains='HNY2018')
 	for mosaic_obj in mosaic_results:
-		mosaic_obj.tags = 'MPEarthDay2018|'
+		mosaic_obj.tags = 'MPHNY2018|'
 		mosaic_obj.save()
-	'''	
+	
 	tag_obj = Tag.objects.get(value=tag)
 	
 	tag_data = {
