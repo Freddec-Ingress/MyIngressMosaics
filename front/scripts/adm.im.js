b403c866d35b57a83250787568633a2adca4a909
freddec <deconinck.frederic@yahoo.fr>
2018-04-15 18:34:50 +0000

    dev

--- front/scripts/adm.im.js
+++ front/scripts/adm.im.js
@@ -1,5 +1,14 @@
 angular.module('FrontModule.controllers').controller('AdmImCtrl', function($scope, API) {
 
+	$scope.clipboardCopy = function(country) {
+		
+		var index = $scope.countries.indexOf(country);
+		var input_country = $('#country_input_' + index);
+		
+		input_country.select();
+		document.execCommand('Copy');
+	}
+	
 	/* Page loading */
 	
 	$scope.loaded = true;
--- static/front.js
+++ static/front.js
@@ -3304,6 +3304,15 @@ angular.module('FrontModule.controllers').controller('AdmRegionCtrl', function($
 });
 angular.module('FrontModule.controllers').controller('AdmImCtrl', function($scope, API) {
 
+	$scope.clipboardCopy = function(country) {
+		
+		var index = $scope.countries.indexOf(country);
+		var input_country = $('#country_input_' + index);
+		
+		input_country.select();
+		document.execCommand('Copy');
+	}
+	
 	/* Page loading */
 	
 	$scope.loaded = true;
