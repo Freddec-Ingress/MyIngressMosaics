4ce67261218820d6a28b201cc3ee489c65f415b7
freddec <deconinck.frederic@yahoo.fr>
2018-04-18 21:13:35 +0000

    dev

--- front/views.py
+++ front/views.py
@@ -1169,7 +1169,7 @@ def adm_missions(request):
 
 	context = { 'missions':[] }
 	
-	mission_results = Mission.objects.filter(mosaic__isnull=True, admin=True, validated=False).order_by('creator', 'name')[:5000]
+	mission_results = Mission.objects.filter(mosaic__isnull=True, admin=True).annotate(num_name=Count('name')).filter(num_name__gte=60)
 	for mission_obj in mission_results:
 		
 		mission_data = {
@@ -1178,7 +1178,7 @@ def adm_missions(request):
 			'name':mission_obj.name,
 			'title':mission_obj.title,
 			'creator':mission_obj.creator,
-			'faction':mission_obj.faction,
+			'excluded':mission_obj.excluded,
 		}
 		
 		context['missions'].append(mission_data)
