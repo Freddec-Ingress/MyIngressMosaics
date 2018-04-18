822fa209d564363f17d7c688e888babd8d7ef1ac
freddec <deconinck.frederic@yahoo.fr>
2018-04-18 10:11:51 +0000

    dev

--- front/scripts/adm.missions.js
+++ front/scripts/adm.missions.js
@@ -1,5 +1,19 @@
 angular.module('FrontModule.controllers').controller('AdmMissionsCtrl', function($scope, API) {
 	
+	/* Mission management */
+	
+	$scope.exclude = function(mission) {
+		
+		var refs = [mission.ref];
+
+		var data = { 'refs':refs };
+		API.sendRequest('/api/potential/exclude/', 'POST', {}, data).then(function(esponse) {
+			
+			var index = $scope.missions.indexOf(mission);
+			$scope.missions.splice(index, 1);
+		});
+	}
+	
 	/* Page loading */
 	
 	$scope.init = function(missions) {
--- static/front.js
+++ static/front.js
@@ -3691,6 +3691,20 @@ angular.module('FrontModule.controllers').controller('AdmImCtrl', function($scop
 });
 angular.module('FrontModule.controllers').controller('AdmMissionsCtrl', function($scope, API) {
 	
+	/* Mission management */
+	
+	$scope.exclude = function(mission) {
+		
+		var refs = [mission.ref];
+
+		var data = { 'refs':refs };
+		API.sendRequest('/api/potential/exclude/', 'POST', {}, data).then(function(esponse) {
+			
+			var index = $scope.missions.indexOf(mission);
+			$scope.missions.splice(index, 1);
+		});
+	}
+	
 	/* Page loading */
 	
 	$scope.init = function(missions) {
