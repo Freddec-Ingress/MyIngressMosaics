c9745d8b81f868238d3b6b8f41430132bb1613fc
freddec <deconinck.frederic@yahoo.fr>
2018-04-18 10:32:13 +0000

    dev

--- front/scripts/adm.missions.js
+++ front/scripts/adm.missions.js
@@ -14,6 +14,30 @@ angular.module('FrontModule.controllers').controller('AdmMissionsCtrl', function
 		});
 	}
 	
+	$scope.remove_creator = function(creator_name) {
+		
+		var refs = [];
+		var missions = [];
+		
+		for (var mission of $scope.missions) {
+			if (mission.creator == creator_name) {
+				
+				refs.push(mission.ref);
+				missions.push(mission);
+			}
+		}
+		
+		var data = { 'refs':refs };
+		API.sendRequest('/api/potential/exclude/', 'POST', {}, data).then(function(esponse) {
+			
+			for (var mission of missions) {
+				
+				var index = $scope.missions.indexOf(mission);
+				$scope.missions.splice(index, 1);
+			}
+		});
+	}
+	
 	/* Page loading */
 	
 	$scope.init = function(missions) {
