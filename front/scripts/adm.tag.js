d22a3b6b29edb229f36ade12ff5cd6d78e604d57
freddec <deconinck.frederic@yahoo.fr>
2018-05-17 15:42:56 +0000

    dev

--- front/scripts/adm.tag.js
+++ front/scripts/adm.tag.js
@@ -1,5 +1,30 @@
 angular.module('FrontModule.controllers').controller('AdmTagCtrl', function($scope, API) {
 	
+	/* Search management */
+	
+	$scope.mosaics = null;
+	
+	$scope.searching = false;
+
+	$scope.search = function(text) {
+		
+		if (!text) return;
+		if (text.length < 3) return;
+		
+		$scope.searching = true;
+		
+		$scope.mosaics = null;
+		
+		var data = { 'text':text };
+		API.sendRequest('/api/search/', 'POST', {}, data).then(function(response) {
+			
+			$scope.mosaics = response.mosaics;
+			$scope.searching = false;
+		});
+	}
+	
+	/* Page loading */
+	
 	$scope.init = function() {
 		
 		$scope.loaded = true;
--- static/front.js
+++ static/front.js
@@ -3961,6 +3961,31 @@ angular.module('FrontModule.controllers').controller('AdmMissionsCtrl', function
 });
 angular.module('FrontModule.controllers').controller('AdmTagCtrl', function($scope, API) {
 	
+	/* Search management */
+	
+	$scope.mosaics = null;
+	
+	$scope.searching = false;
+
+	$scope.search = function(text) {
+		
+		if (!text) return;
+		if (text.length < 3) return;
+		
+		$scope.searching = true;
+		
+		$scope.mosaics = null;
+		
+		var data = { 'text':text };
+		API.sendRequest('/api/search/', 'POST', {}, data).then(function(response) {
+			
+			$scope.mosaics = response.mosaics;
+			$scope.searching = false;
+		});
+	}
+	
+	/* Page loading */
+	
 	$scope.init = function() {
 		
 		$scope.loaded = true;
