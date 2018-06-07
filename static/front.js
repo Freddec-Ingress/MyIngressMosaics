bc0cebc8b38ed6a77f219355c5c33d7a7fdf6481
freddec <deconinck.frederic@yahoo.fr>
2018-06-07 11:57:48 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -1209,7 +1209,7 @@ angular.module('FrontModule.controllers').controller('MosaicPageCtrl', function(
 			scaledSize: new google.maps.Size(50, 50),
 			origin: new google.maps.Point(0, 0),
 			anchor: new google.maps.Point(25, 25),
-			labelOrigin: new google.maps.Point(25, 22),
+			labelOrigin: new google.maps.Point(25, 28),
 			url: 'https://www.myingressmosaics.com/static/img/neutral.png',
 		};
 
