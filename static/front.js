5101739df15007f13d738355e43a84b9daf5ffca
freddec <deconinck.frederic@yahoo.fr>
2018-06-27 14:48:18 +0000

    dev

--- static/front.js
+++ static/front.js
@@ -3577,17 +3577,37 @@ angular.module('FrontModule.controllers').controller('ManagePageCtrl', function(
 });
 angular.module('FrontModule.controllers').controller('WaitingPageCtrl', function($scope) {
     
+	/* Waiting management */
+	
+	$scope.getImage = function(index) {
+		for (var mission of $scope.missions) {
+			if (mission.order == index) {
+				return mission.image;
+			}
+		}
+		var i = $scope.missing_missions.indexOf(index);
+		if (i == -1) {
+			$scope.missing_missions += index + ', ';
+		}
+		return null;
+	}
+	
 	/* Tab management */
 	
 	$scope.current_tab = 'details';
     
 	/* Page init */
 	
-	$scope.init = function(mosaic, missions) {
+	$scope.init = function(waiting, missions) {
 
-		$scope.mosaic = mosaic;
+		$scope.waiting = waiting;
 		$scope.missions = missions;
 		
+		$scope.range = [];
+	    for (var i = 0; i < $scope.mission_count; i++) {
+	    	$scope.range.push(i);
+	    }
+
 		$scope.loaded = true;
 	}
 });
