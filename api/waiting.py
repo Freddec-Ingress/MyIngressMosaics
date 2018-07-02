e993ecd87000db9b300a7a102b30c9e49dea4df1
freddec <deconinck.frederic@yahoo.fr>
2018-07-02 08:37:22 +0000

    dev

--- api/waiting.py
+++ api/waiting.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
+from django.db.models import Q
+
 from .models import *
 
 
