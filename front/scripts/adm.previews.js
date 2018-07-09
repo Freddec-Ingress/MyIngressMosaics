847a7836aedf27c85b01e9ac58dd05de069eb3b6
freddec <deconinck.frederic@yahoo.fr>
2018-07-09 11:23:56 +0000

    dev

--- front/scripts/adm.previews.js
+++ front/scripts/adm.previews.js
@@ -4,6 +4,22 @@ angular.module('FrontModule.controllers').controller('AdmPreviewsCtrl', function
 	
 	$scope.current_tab = '';
 	
+	/* Preview management */
+
+    var to_generate = [];
+
+	var generate = function() {
+	    
+	    if (to_generate.length < 1) return;
+	    
+		var data = { 'ref':to_generate[0].ref,  };
+		API.sendRequest('/api/mosaic/preview/generate/', 'POST', {}, data).then(function(response) {
+		    
+		    to_generate = to_generate.splice(0, 1);
+		    generate();
+		});
+	}
+	
 	/* Cleaning management */
 	
 	$scope.cleaning_chekcing = false;
@@ -20,6 +36,12 @@ angular.module('FrontModule.controllers').controller('AdmPreviewsCtrl', function
     	    $scope.to_be_cleaned = response.mosaics;
     	    $scope.cleaning_chekcing = false;
 		});
+	}
+	
+	$scope.generateCleaning = function() {
+	    
+	    to_generate = $scope.to_be_cleaned;
+	    generate();
 	}
 	
 	/* Page loading */
--- static/front.js
+++ static/front.js
@@ -4317,6 +4317,22 @@ angular.module('FrontModule.controllers').controller('AdmPreviewsCtrl', function
 	
 	$scope.current_tab = '';
 	
+	/* Preview management */
+
+    var to_generate = [];
+
+	var generate = function() {
+	    
+	    if (to_generate.length < 1) return;
+	    
+		var data = { 'ref':to_generate[0].ref,  };
+		API.sendRequest('/api/mosaic/preview/generate/', 'POST', {}, data).then(function(response) {
+		    
+		    to_generate = to_generate.splice(0, 1);
+		    generate();
+		});
+	}
+	
 	/* Cleaning management */
 	
 	$scope.cleaning_chekcing = false;
@@ -4333,6 +4349,12 @@ angular.module('FrontModule.controllers').controller('AdmPreviewsCtrl', function
     	    $scope.to_be_cleaned = response.mosaics;
     	    $scope.cleaning_chekcing = false;
 		});
+	}
+	
+	$scope.generateCleaning = function() {
+	    
+	    to_generate = $scope.to_be_cleaned;
+	    generate();
 	}
 	
 	/* Page loading */
